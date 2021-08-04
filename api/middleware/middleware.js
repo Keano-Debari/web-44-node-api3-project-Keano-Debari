eb7c5842d69988ac74748e2eefc0aff14c14da67
unknown <Keano@Firelink.Firelink>
2021-08-04 14:36:06 -0700

    mvp

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,49 @@
+const User = require('../users/users-model')
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log(`[${new Date().toLocaleString()}] ${req.method} to ${req.url}`)
+  next()
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  try {
+    const user = await User.getById(req.params.id)
+    if(user) {
+      req.user = user
+      next()
+    }
+    else {
+      next({status: 404, message: "user not found"})
+    }
+  }
+  catch (err) {
+    res.status(500).json({message: 'cant find user'})
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  const { name } = req.body
+  if(!name) {
+    req.status(400).json({message: "missing required name field"})
+  }
+  else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  const { text } = req.body
+  if(!text) {
+    req.status(400).json({message: "missing required text field"})
+  }
+  else {
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
+module.exports = { logger, validateUserId, validateUser, validatePost }
--- api/server.js
+++ api/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const usersRouter = require('./users/users-router');
+const { logger } = require('./middleware/middleware');
 
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
 
+server.use(express.json());
+
 // global middlewares and the user's router need to be connected here
 
-server.get('/', (req, res) => {
+server.use('/api/users', [logger], usersRouter);
+
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
